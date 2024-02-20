fde684d3545d83c0f3106a292aa048318a3d9daa
Rihab Zamouri <rihabzamouri@Rihabs-MacBook-Pro.local>
2024-02-20 23:06:01 +0000

    test1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to React
+          Welcome to React Rihab
         </a>
       </header>
     </div>
